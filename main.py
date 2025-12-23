3cfd7c3b24536a9de57fe8d52c0be9f9298e8c34
Emmanuele Levi <ignisvictorious@gmail.com>
2025-12-23 22:33:58 +0000

    basic auth is working

--- main.py
+++ main.py
@@ -1,27 +1,44 @@
-
-# 
+#
 #  Import LIBRARIES
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+
 #  Import FILES
-# 
+#
 #  ______________________
-
-
-
 
 
 app = FastAPI()
 
-@app.get("/")
-def main():
-    return {"message": "Hello World"}
+security = HTTPBasic()
 
 
+#  Credentials
+username: str = "Tony Soprano"
+password: str = "mafia"
 
 
-# 
+@app.get(path="/")
+def secret_response(credentials: Annotated[HTTPBasicCredentials, Depends(dependency=security)]) -> dict[str, str]:
+    if credentials.username == username and credentials.password == password:
+        return {"secret": "this is highly secret.."}
+
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Incorrect username or password",
+        headers={"WWW-Authenticate": "Basic"},
+    )
+
+
+# @app.get(path="/")
+# def main() -> dict[str, str]:
+#     return {"message": "Hello World"}
+
+
+#
 #  Import LIBRARIES
 #  Import FILES
-# 
+#
 #  ______________________
-
