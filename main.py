5a2b2a93e9efaaac18168efa2f2397b50ad9555f
Emmanuele Levi <ignisvictorious@gmail.com>
2025-12-23 23:37:31 +0000

    add bcrypt

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 #  Import LIBRARIES
 from typing import Annotated
 
+import bcrypt
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
@@ -18,11 +19,16 @@ security = HTTPBasic()
 #  Credentials
 username: str = "Tony Soprano"
 password: str = "mafia"
+hash: bytes = bcrypt.hashpw(password=password.encode(encoding="utf-8"), salt=bcrypt.gensalt())
+print(hash)
 
 
 @app.get(path="/")
 def secret_response(credentials: Annotated[HTTPBasicCredentials, Depends(dependency=security)]) -> dict[str, str]:
-    if credentials.username == username and credentials.password == password:
+    passwords_match: bool = bcrypt.checkpw(password=credentials.password.encode(encoding="utf-8"), hashed_password=hash)
+
+    if credentials.username == username and passwords_match:
+        # if credentials.username == username and credentials.password == password:
         return {"secret": "this is highly secret.."}
 
     raise HTTPException(
